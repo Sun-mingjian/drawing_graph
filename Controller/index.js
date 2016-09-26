04c3421efb63806648f182146428830a1f2a343c
Mingjian_Sun <pythonsun1234@gmail.com>
2016-09-26 23:56:36 +1000

    Angular js practice demo

--- Controller/index.js
+++ Controller/index.js
@@ -1,20 +1,75 @@
-var app = angular.module("app",[]).
-controller('final', ['$scope', function($scope) {
-      $scope.submit = function() {
-        if ($scope.graphType) {
-          alert($scope.graphType);
+var app = angular.module("app", []);
+app.controller('ctrl', ['$scope', function ($scope) {
+
+        $scope.user1 = {
+            name: 'michael',
+            age: 27,
+            gender: 'm',
+            address: {
+                street: 'liverpool street',
+                city: 'sydney',
+                country: 'Australia'
+            },
+            friends: [
+           "lida", "jack", "brad"
+       ]
+
         }
-      };
-    $scope.graphDiv = "graphDiv";
-    
-    
-      }
+        $scope.user2 = {
+            name: 'Jenny',
+            age: 27,
+            gender: 'm',
+            address: {
+                street: 'liverpool street',
+                city: 'sydney',
+                country: 'Australia'
+            },
+            friends: [
+           "lida", "jack", "brad"
+       ]
+
+        }
+}
 ]);
 
 
-app.directive('graph', function () {
+app.directive('userInfoCard', function () {
     return {
-        templateUrl: 'View/graph.html',
-        restrict:'E'
+        templateUrl: 'View/userInfoCard.html',
+        restrict: 'E',
+        scope: {
+            user: "="
+        },
+        controller: function ($scope) {
+            $scope.collapsed = false;
+            $scope.getRank = function (user) {
+                user.rank = "99";
+            }
+            $scope.collapse = function () {
+                $scope.collapsed = !$scope.collapsed;
+            }
+
+        }
+    }
+});
+
+
+
+app.directive('address', function () {
+    return {
+        templateUrl: 'View/address.html',
+        restrict: 'E',
+        scope: true,
+        controller: function ($scope) {
+            $scope.collapsed = true;
+            $scope.expandAddress = function () {
+                $scope.collapsed = true;
+            }
+            $scope.collapseAddress = function () {
+
+                $scope.collapsed = false;
+            }
+
+        }
     }
 });
